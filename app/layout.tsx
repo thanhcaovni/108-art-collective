ac15bce8eb56f3684b277605929a38684a109928
thanhcaovni <thanhcao.vni@gmail.com>
2026-09-15 21:33:31 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import MusicPlayer from "@/components/MusicPlayer";
+import MusicPlayer from "../components/MusicPlayer";
 
 export const metadata: Metadata = {
   title: "108 Art Collective",
