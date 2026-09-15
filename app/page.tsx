71191683dfba57354a0eb240cc9cd184810b3b08
thanhcaovni <thanhcao.vni@gmail.com>
2026-09-15 13:24:19 +0700

    Refactor Home component layout and add imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Hero from "../components/Hero";
+import Intro from "../components/Intro";
+
 export default function Home() {
   return (
-    <main className="w-screen h-svh bg-black">
-      <h1 className="text-white text-2xl">108 ART COLLECTIVE</h1>
+    <main className="bg-black min-h-svh overflow-hidden">
+      <Intro />
+      <Hero />
     </main>
   );
 }
