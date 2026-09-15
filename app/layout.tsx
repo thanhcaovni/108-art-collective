6dc2e0cda4f4ede32176b1e7c53b769e544715a8
thanhcaovni <thanhcao.vni@gmail.com>
2026-09-15 21:31:16 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import MusicPlayer from "@/components/MusicPlayer";
 
 export const metadata: Metadata = {
   title: "108 Art Collective",
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning>{children}</body>
+      <body suppressHydrationWarning>
+        <MusicPlayer />
+        {children}
+      </body>
     </html>
   );
 }
