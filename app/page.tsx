12c9bffe961d67371c196e07a2e435f42edbc24f
thanhcaovni <thanhcao.vni@gmail.com>
2026-09-15 15:04:23 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Hero from "../components/Hero";
-import Intro from "../components/Intro";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="bg-black min-h-svh overflow-hidden">
-      <Intro />
+    <main className="bg-black">
       <Hero />
     </main>
   );
